e504c97e182326df962fc5eb2e1403de5f1b00e1
Yatin Jain <rokstaryatin2012@gmail.com>
2021-05-24 19:23:40 +0530

    A visual tweak

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 
 function HeaderCartButton(props) {
+    const [btnAnimation, setBtnAnimation] = useState(false);
     const cartCtx = useContext(CartContext);
-    const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
-        return currNumber + item.quantity
-    }, 0);
+    const { items } = cartCtx;
+    const numberOfCartItems = items.reduce((currNumber, item) => currNumber + item.quantity, 0);
+    const btnClasses = `${styles.button} ${btnAnimation ? styles.bump : ''}`;
+    useEffect(() => {
+        if(items.length===0) return;
+        setBtnAnimation(true);
+        const timer = setTimeout(() => setBtnAnimation(false), 300);
+        return () => clearTimeout(timer);
+    }, [items]);
     return (
-        <button className={styles.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={styles.icon}><CartIcon /></span>
             <span>Your Cart</span>
             <span className={styles.badge}>{numberOfCartItems}</span>
